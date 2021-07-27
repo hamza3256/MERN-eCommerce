8a5bc9152127a2261fb64112fb3c3151b363ec11
HAMZA <hamza3256@live.co.uk>
2021-07-27 17:31:26 +0100

    Add: display user in navbar when logged in

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,18 +1,16 @@
-import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import React from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
 
 const Footer = () => {
-    return (
-        <footer>
-        <Container>
-            <Row>
-                <Col className='text-center py3'>
-                    Copyright &copy; AdamJee Mart
-                </Col>
-            </Row>
-        </Container>
-        </footer>
-    )
-}
+  return (
+    <footer>
+      <Container>
+        <Row>
+          <Col className='text-center py2'>Copyright &copy; AdamJee Mart</Col>
+        </Row>
+      </Container>
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
