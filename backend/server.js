5706df5b61b98129687a48eb1961c039c71d112d
HAMZA <hamza3256@live.co.uk>
2021-08-05 18:02:04 +0100

    Fix: bugs, prepare for heroku deployment

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,6 @@ connectDB();
 const app = express();
 
 app.use(express.json());
-//app.use(express.urlencoded({ extended: true }));
 
 app.use('/api/products', productRoutes);
 app.use('/api/users', userRoutes);
